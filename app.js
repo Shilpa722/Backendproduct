1a4f139be524ac01f0d6f75b8a0e2a2246a7aa53
Shilpa Hallur <shilpahallur5@gmail.com>
2026-01-07 13:12:35 +0530

    Your commit message

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express=require('express');
-const mongoose=require('mongoose');
+const cors=require('cors')
 const app=express();
 app.use(express.json())
+app.use(cors());
+let limiter=require('./src/middlewares/ratelimit')
+app.use(limiter)
 let statusrouter=require('./src/routes/status.routes')
 let factrouter=require('./src/routes/fact.routes')
 let productrouter=require('./src/routes/product.route')
